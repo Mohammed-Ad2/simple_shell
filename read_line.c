064d94a8c2964ecbbd6bbcb06d3bd0be777482d9
Adam <simoaddame17@gmail.com>
2024-02-23 12:33:31 +0100

    getline

---
+++ read_line.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * read_line - Reads a line of input from the standard input.
+ *
+ * Returns:
+ *   - On success, returns a pointer to the buffer containing the line read.
+ *   - If an error occurs or end-of-file is reached, returns NULL.
+ */
+char *read_line(void)
+{
+    char *buffer = NULL;
+    size_t size_buffer = 0;
+    ssize_t n_char;
+
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, "$ ", 2);
+    n_char = getline(&buffer, &size_buffer, stdin);
+    if (n_char == -1)
+    {
+        free(buffer);
+        return (NULL);
+    }
+
+    if (buffer[n_char - 1] == '\n')
+        buffer[n_char - 1] = '\0';
+
+    return (buffer);
+}
