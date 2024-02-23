3a6d9b546557db4f48be486306bc094033d9013d
Adam <simoaddame17@gmail.com>
2024-02-23 12:46:18 +0100

    all

--- s_shell.h
+++ s_shell.h
@@ -1,18 +1,18 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
+
 #include <sys/stat.h>
 #include <errno.h>
-
+#include <fcntl.h>
 
 char *read_line(void);
-char *mystrdup(char *str);
+char *_strdup(char *str);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
