52118e5d2158e0dff52d668451e6b5f539772e89
Adam <simoaddame17@gmail.com>
2024-02-25 22:27:50 +0100

    all

--- main.c
+++ main.c
@@ -7,31 +7,26 @@
 */
 int main(int ac, char **av)
 {
-    char **command = NULL;
-    char *line = NULL;
-    int i, status = 0;
-    (void) ac;
-    (void) av;
+		char **command = NULL;
+			char *line = NULL;
+				int i, status = 0;
+					(void) ac;
+						(void) av;
 
-    while (1)
-    {
-        line = read_line();
-        if (line == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            return (status);
-        }
+							while (1)
+									{
+												line = read_line();
+														if (line == NULL)
+																	{
+																					if (isatty(STDIN_FILENO))
+																										write(STDOUT_FILENO, "\n", 1);
+																								return (status);
+																										}
 
-        command = splitter(line);
-        if (!command)
-            continue;
+																command = splitter(line);
+																		if (!command)
+																						continue;
 
-        for (i = 0; command[i]; i++)
-        {
-            printf("%s\n", command[i]);
-            free(command[i]), command[i] = NULL;
-        }
-        free(command), command = NULL;
-    }
+																				/* status = executer(command, av); */
+																			}
 }
