6eb1540ac3062624bbd65be5cc1de683b9e70c80
Adam <simoaddame17@gmail.com>
2024-02-26 22:54:47 +0100

    Entry point

--- main.c
+++ main.c
@@ -1,32 +1,34 @@
 #include "s_shell.h"
 
 /**
- * main - entry point
+ * main - Entry point of the shell program.
  *
- * return: 
-*/
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of strings containing the arguments passed to the program.
+ *
+ * Return: The exit status of the shell program.
+ */
 int main(int ac, char **av)
 {
-		char **command = NULL;
-			char *line = NULL;
-				int i, status = 0;
-					(void) ac;
-						(void) av;
+	char **cmd = NULL, *line = NULL;
+	int status = 0;
+	(void) ac;
+	(void) av;
 
-							while (1)
-									{
-												line = read_line();
-														if (line == NULL)
-																	{
-																					if (isatty(STDIN_FILENO))
-																										write(STDOUT_FILENO, "\n", 1);
-																								return (status);
-																										}
+	while (1)
+	{
+		line = read_line();
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (status);
+		}
 
-																command = splitter(line);
-																		if (!command)
-																						continue;
+		cmd = splitter(line);
+		if (!cmd)
+			continue;
 
-																				/* status = executer(command, av); */
-																			}
+		status = _execute(cmd, av);
+	}
 }
