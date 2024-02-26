745493b0b7a0e6f04b67b4a9ee16deea4e360dda
Adam <simoaddame17@gmail.com>
2024-02-26 23:14:48 +0100

    execute

---
+++ ext.c
@@ -0,0 +1,32 @@
+#include "s_shell"
+
+/**
+ * _execute - Executes a command with arguments.
+ * @cmd: An array of strings representing the command and its arguments.
+ * @av: An array of strings containing the program name and its arguments.
+ *
+ * Return: The exit status of the executed command.
+ */
+int _execute(char **cmd, char **av)
+{
+	int status;
+	pid_t child;
+
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(cmd[0], cmd, environ))
+		{
+			perror(av[0]);
+			free_2Darr(cmd);
+			exit(127);
+		}
+	}
+	else
+	{
+		waitpid(child, &status, 0);
+		free_2Darr(cmd);
+	}
+
+	return (WEXITSTATUS(status));
+}
