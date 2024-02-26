20e166912c05c7492940b0f54e09530396aa6871
Adam <simoaddame17@gmail.com>
2024-02-26 23:17:11 +0100

    Header file

--- s_shell.h
+++ s_shell.h
@@ -1,15 +1,17 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef S_SHELL_H
+#define S_SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
+
+extern char **environ;
 
 char *read_line(void);
 char *_strdup(char *str);
